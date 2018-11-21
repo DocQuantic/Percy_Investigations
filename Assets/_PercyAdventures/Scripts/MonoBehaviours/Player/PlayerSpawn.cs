656fd98f8fba61758c09a391b2eaa01d5ef4051c
DocQuantic <w.magrini@yahoo.fr>
2018-11-21 09:38:06 +0100

    Added player and camera spawn to all scenes

---
+++ Assets/_PercyAdventures/Scripts/MonoBehaviours/Player/PlayerSpawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour {
+
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        player.position = transform.position;
+        player.rotation = transform.rotation;
+    }
+
+}
